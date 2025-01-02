6b546768681f40e7fd3de9b97270125357003e82
Cristian <Cristian.riano@mycsolutions.com.co>
2025-01-02 13:12:57 +0100

    Se define el tipado de los parámetros

--- muric-entities/src/main/java/co/com/muric/entities/model/Entidad.java
+++ muric-entities/src/main/java/co/com/muric/entities/model/Entidad.java
@@ -1,10 +1,14 @@
 package co.com.muric.entities.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -20,39 +24,41 @@ public class Entidad {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(max = 50)
-    @Column(name = "TIPO_ENTIDAD", length = 50)
+    @Size(max = 200)
+    @Column(name = "TIPO_ENTIDAD", length = 200)
     private String tipoEntidad;
 
-    @Size(max = 50)
-    @Column(name = "CODIGO_ENTIDAD", length = 50)
+    @Size(max = 200)
+    @Column(name = "CODIGO_ENTIDAD", length = 200)
     private String codigoEntidad;
 
-    @Size(max = 50)
-    @Column(name = "NOMBRE_ENTIDAD", length = 50)
+    @Size(max = 200)
+    @Column(name = "NOMBRE_ENTIDAD", length = 200)
     private String nombreEntidad;
 
-    @Size(max = 50)
-    @Column(name = "TIPO_REPORTE", length = 50)
+    @Size(max = 200)
+    @Column(name = "TIPO_REPORTE", length = 200)
     private String tipoReporte;
 
-    @Size(max = 50)
-    @Column(name = "FECHA_CORTE", length = 50)
-    private String fechaCorte;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "FECHA_CORTE")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date fechaCorte;
 
-    @Size(max = 50)
-    @Column(name = "FECHA_GENERACION", length = 50)
+    @Temporal(TemporalType.DATE)
+    @Column(name = "FECHA_GENERACION")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private String fechaGeneracion;
 
-    @Size(max = 50)
-    @Column(name = "COMENTARIOS", length = 50)
+    @Size(max = 500)
+    @Column(name = "COMENTARIOS", length = 500)
     private String comentarios;
 
-    @Size(max = 50)
-    @Column(name = "FIRMA", length = 50)
+    @Size(max = 200)
+    @Column(name = "FIRMA", length = 200)
     private String firma;
 
-    @Size(max = 50)
-    @Column(name = "PALABRA_CLAVE", length = 50)
+    @Size(max = 200)
+    @Column(name = "PALABRA_CLAVE", length = 200)
     private String palabraClave;
 }
