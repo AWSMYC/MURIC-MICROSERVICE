07c918672cc96951761dd73ff6fc58ac75344c0f
Cristian <Cristian.riano@mycsolutions.com.co>
2025-01-02 13:20:23 +0100

    Se define el tipado de los parámetros de InsumosCreditos

--- muric-entities/src/main/java/co/com/muric/entities/model/InsumosCredito.java
+++ muric-entities/src/main/java/co/com/muric/entities/model/InsumosCredito.java
@@ -1,10 +1,14 @@
 package co.com.muric.entities.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -20,63 +24,65 @@ public class InsumosCredito {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(max = 50)
-    @Column(name = "IDENTIFICACION_CREDITO_ENTIDAD", length = 50)
+    @Size(max = 200)
+    @Column(name = "IDENTIFICACION_CREDITO_ENTIDAD", length = 200)
     private String identificacionCreditoEntidad;
 
-    @Size(max = 50)
-    @Column(name = "TIPO_IDENTIFICACION", length = 50)
+    @Size(max = 200)
+    @Column(name = "TIPO_IDENTIFICACION", length = 200)
     private String tipoIdentificacion;
 
-    @Size(max = 50)
-    @Column(name = "NUMERO_INDENTIFICACION", length = 50)
+    @Size(max = 200)
+    @Column(name = "NUMERO_INDENTIFICACION", length = 200)
     private String numeroIdentificacion;
 
-    @Size(max = 50)
-    @Column(name = "MODALIDAD", length = 50)
+    @Size(max = 200)
+    @Column(name = "MODALIDAD", length = 200)
     private String modalidad;
 
-    @Size(max = 50)
-    @Column(name = "CODIGO_PRODUCTO", length = 50)
+    @Size(max = 200)
+    @Column(name = "CODIGO_PRODUCTO", length = 200)
     private String codigoProducto;
 
-    @Size(max = 50)
-    @Column(name = "CALIDAD_DEUDOR", length = 50)
+    @Size(max = 200)
+    @Column(name = "CALIDAD_DEUDOR", length = 200)
     private String calidadDeudor;
 
-    @Size(max = 50)
-    @Column(name = "FECHA_DESEMBOLSO", length = 50)
-    private String fechaDesembolso;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "FECHA_DESEMBOLSO")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date fechaDesembolso;
 
-    @Size(max = 50)
-    @Column(name = "FECHA_VENCIMIENTO", length = 50)
-    private String fechaVencimiento;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "FECHA_VENCIMIENTO")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date fechaVencimiento;
 
-    @Size(max = 50)
-    @Column(name = "VALOR_DESEMBOLSADO", length = 50)
-    private String valorDesembolsado;
+    @Column(name = "VALOR_DESEMBOLSADO", nullable = false, precision = 10, scale = 2)
+    @Digits(integer = 10, fraction = 2, message = "El valor debe tener hasta 12 dígitos enteros y 2 decimales")
+    private Double valorDesembolsado;
 
-    @Size(max = 50)
-    @Column(name = "FRECUENCIA_PAGO_CAPITAL", length = 50)
+    @Size(max = 200)
+    @Column(name = "FRECUENCIA_PAGO_CAPITAL", length = 200)
     private String frecuenciaPagoCapital;
 
-    @Size(max = 50)
-    @Column(name = "FRECUENCIA_PAGO_INTERESES", length = 50)
+    @Size(max = 200)
+    @Column(name = "FRECUENCIA_PAGO_INTERESES", length = 200)
     private String frecuenciaPagoIntereses;
 
-    @Size(max = 50)
-    @Column(name = "TIPO_TASA", length = 50)
+    @Size(max = 200)
+    @Column(name = "TIPO_TASA", length = 200)
     private String tipoTasa;
 
-    @Size(max = 50)
-    @Column(name = "TIPO_GARANTIA", length = 50)
+    @Size(max = 200)
+    @Column(name = "TIPO_GARANTIA", length = 200)
     private String tipoGarantia;
 
-    @Size(max = 50)
-    @Column(name = "MONEDA", length = 50)
+    @Size(max = 200)
+    @Column(name = "MONEDA", length = 200)
     private String moneda;
 
-    @Size(max = 50)
-    @Column(name = "ESTADO_REGISTRO", length = 50)
-    private String esatdoRegistro;
+    @Size(max = 200)
+    @Column(name = "ESTADO_REGISTRO", length = 200)
+    private String estadoRegistro;
 }
