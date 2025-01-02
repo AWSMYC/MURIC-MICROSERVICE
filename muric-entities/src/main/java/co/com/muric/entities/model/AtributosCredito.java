e97e50f930cb83f2c7c0b84bd6e2620a4f16173d
Cristian <Cristian.riano@mycsolutions.com.co>
2025-01-02 13:21:44 +0100

    Se define el tipado de los parámetros de AtributosCredito

--- muric-entities/src/main/java/co/com/muric/entities/model/AtributosCredito.java
+++ muric-entities/src/main/java/co/com/muric/entities/model/AtributosCredito.java
@@ -2,6 +2,7 @@ package co.com.muric.entities.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -36,8 +37,8 @@ public class AtributosCredito {
     @Column(name = "CLAVE_ATRIBUTO", length = 50)
     private String claveAtributo;
 
-    @Size(max = 50)
-    @Column(name = "VALOR_ATRIBUTO", length = 50)
-    private String valorAtributo;
+    @Column(name = "VALOR_ATRIBUTO", nullable = false, precision = 10, scale = 2)
+    @Digits(integer = 10, fraction = 2, message = "El valor debe tener hasta 12 dígitos enteros y 2 decimales")
+    private Double valorAtributo;
 
 }
