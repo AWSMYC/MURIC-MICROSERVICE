ce8b39725807d6045c93383840a3f2a82e268a52
Cristian <Cristian.riano@mycsolutions.com.co>
2025-03-01 18:06:06 +0100

    Se implementa la logica de validación de los RequestParam sources & type ©kristian-hernandez

--- muric-web/src/main/java/co/com/muric/web/rest/MuricController.java
+++ muric-web/src/main/java/co/com/muric/web/rest/MuricController.java
@@ -7,9 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.text.MessageFormat;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
