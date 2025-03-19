450cd7a8683de85b62de914cf442ca934a28438d
Cristian <Cristian.riano@mycsolutions.com.co>
2025-03-19 13:36:06 +0100

    Se obtienen los datos desde la base de datos - ©Kristian Hernandez

--- muric-infrastructure/src/main/java/co/com/muric/infrastructure/db/implement/ConnectionDataBase.java
+++ muric-infrastructure/src/main/java/co/com/muric/infrastructure/db/implement/ConnectionDataBase.java
@@ -39,19 +39,63 @@ public class ConnectionDataBase implements IConnectionDataBase {
                 while (resultSet.next()) {
                     switch (tableName) {
                         case StaticVariables.INFORMACION_CREDITO_ENTIDAD_TABLE_NAME:
-                            informacionCreditoList.add(InformacionCredito.builder().build());
-                            String codigo_producto = resultSet.getString("codigo_producto");
-                            System.out.println("---------------" + codigo_producto + "----------------");
+                            informacionCreditoList.add(InformacionCredito.builder()
+                                    .identificacionCreditoEntidad(resultSet.getString(StaticVariables.IDENTIFICACION_CREDITO_ENTIDAD))
+                                    .tipoIdentificacion(resultSet.getInt(StaticVariables.TIPO_IDENTIFICACION))
+                                    .numeroIdentificacion(resultSet.getString(StaticVariables.NUMERO_IDENTIFICACION))
+                                    .modalidad(resultSet.getInt(StaticVariables.MODALIDAD))
+                                    .codigoProducto(resultSet.getInt(StaticVariables.CODIGO_PRODUCTO))
+                                    .calidadDeudor(resultSet.getInt(StaticVariables.CALIDAD_DEUDOR))
+                                    .fechaDesembolso(resultSet.getInt(StaticVariables.FECHA_DESEMBOLSO))
+                                    .fechaVencimiento(resultSet.getInt(StaticVariables.FECHA_VENCIMIENTO))
+                                    .valorDesembolsado(resultSet.getFloat(StaticVariables.VALOR_DESEMBOLSADO))
+                                    .frecuenciaPagoCapital(resultSet.getInt(StaticVariables.FRECUENCIA_PAGO_CAPITAL))
+                                    .frecuenciaPagoIntereses(resultSet.getInt(StaticVariables.FRECUENCIA_PAGO_INTERESES))
+                                    .tipoTasa(resultSet.getString(StaticVariables.TIPO_TASA))
+                                    .tipoGarantia(resultSet.getInt(StaticVariables.TIPO_GARANTIA))
+                                    .moneda(resultSet.getString(StaticVariables.MONEDA))
+                                    .estadoRegistro(resultSet.getString(StaticVariables.ESTADO_REGISTRO))
+                                    .build());
                             break;
                         case StaticVariables.ATRIBUTOS_CREDITOS_TABLE_NAME:
-                            atributoCreditoDeudaList.add(AtributoCreditoDeuda.builder().build());
-                            String valor_atributo = resultSet.getString("valor_atributo");
-                            System.out.println("---------------" + valor_atributo + "----------------");
+                            atributoCreditoDeudaList.add(AtributoCreditoDeuda.builder()
+                                    .identificacionCreditoEntidad(resultSet.getString(StaticVariables.IDENTIFICACION_CREDITO_ENTIDAD))
+                                    .tipoIdentificacion(resultSet.getInt(StaticVariables.TIPO_IDENTIFICACION))
+                                    .numeroIdentificacion(resultSet.getString(StaticVariables.NUMERO_IDENTIFICACION))
+                                    .claveAtributo(resultSet.getInt(StaticVariables.CLAVE_ATRIBUTO))
+                                    .valorAtributo(resultSet.getString(StaticVariables.VALOR_ATRIBUTO))
+                                    .build());
                             break;
                         case StaticVariables.MOVIMIENTOS_CARTERA_TABLE_NAME:
-                            movimientoCarteraList.add(MovimientoCartera.builder().build());
-                            String estado = resultSet.getString("estado");
-                            System.out.println("---------------" + estado + "----------------");
+                            movimientoCarteraList.add(MovimientoCartera.builder()
+                                    .identificacionCreditoEntidad(resultSet.getString(StaticVariables.IDENTIFICACION_CREDITO_ENTIDAD))
+                                    .tipoIdentificacion(resultSet.getInt(StaticVariables.TIPO_IDENTIFICACION))
+                                    .numeroIdentificacion(resultSet.getString(StaticVariables.NUMERO_IDENTIFICACION))
+                                    .fechaCorte(resultSet.getInt(StaticVariables.FECHA_CORTE))
+                                    .calificacionCredito(resultSet.getString(StaticVariables.CALIFICACION_CREDITO))
+                                    .estado(resultSet.getInt(StaticVariables.ESTADO))
+                                    .periodoGracia(resultSet.getInt(StaticVariables.PERIODO_GRACIA))
+                                    .diasMora(resultSet.getInt(StaticVariables.DIAS_MORA))
+                                    .tasaInteres(resultSet.getFloat(StaticVariables.TASA_INTERES))
+                                    .spreadTasaInteres(resultSet.getFloat(StaticVariables.SPREAD_TASA_INTERES))
+                                    .saldoCapital(resultSet.getFloat(StaticVariables.SALDO_CAPITAL))
+                                    .saldoIntereses(resultSet.getFloat(StaticVariables.SALDO_INTERESES))
+                                    .saldoOtros(resultSet.getFloat(StaticVariables.SALDO_OTROS))
+                                    .modeloProvisiones(resultSet.getInt(StaticVariables.MODELO_PROVISIONES))
+                                    .provisionProciclica(resultSet.getFloat(StaticVariables.PROVISION_PROCICLICA))
+                                    .provisionContraciclica(resultSet.getFloat(StaticVariables.PROVISION_CONTRACICLICA))
+                                    .provisionAdicionalPoliticaEntidad(resultSet.getFloat(StaticVariables.PROVISION_ADICIONAL_POLITICA_ENTIDAD))
+                                    .provisionOtros(resultSet.getFloat(StaticVariables.PROVISION_OTROS))
+                                    .provisionTotal(resultSet.getFloat(StaticVariables.PROVISION_TOTAL))
+                                    .cuotaEsperadaCapital(resultSet.getFloat(StaticVariables.CUOTA_ESPERADA_CAPITAL))
+                                    .cuotaEsperadaIntereses(resultSet.getFloat(StaticVariables.CUOTA_ESPERADA_INTERESES))
+                                    .cuotaRecibidaCapital(resultSet.getFloat(StaticVariables.CUOTA_RECIBIDA_CAPITAL))
+                                    .cuotaRecibidaIntereses(resultSet.getFloat(StaticVariables.CUOTA_RECIBIDA_INTERESES))
+                                    .valorGarantia(resultSet.getFloat(StaticVariables.VALOR_GARANTIA))
+                                    .fechaGarantia(resultSet.getInt(StaticVariables.FECHA_GARANTIA))
+                                    .probabilidadIncumplimientoCredito(resultSet.getFloat(StaticVariables.PROBABILIDAD_INCUMPLIMIENTO_CREDITO))
+                                    .perdidaDadoIncumplimiento(resultSet.getFloat(StaticVariables.PROBABILIDAD_INCUMPLIMIENTO_CREDITO))
+                                    .build());
                             break;
                     }
                 }
